b8c22aa63a13958038f8704550864187e19acaa5
Dhruvil Prajapati <dhruvilprajapati2003@gmail.com>
2023-10-15 22:04:45 +0530

    Added files and changes required for deployment

--- photobooth/settings.py
+++ photobooth/settings.py
@@ -13,29 +13,33 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import sys
-sys.path.append('/myenv/src/django-verify-email/verify-email')
+import dj_database_url
+
+sys.path.append("/myenv/src/django-verify-email/verify-email")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 from dotenv import load_dotenv
+
 load_dotenv()
-
-
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1",]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,13 +48,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     # "photos",
     "django.contrib.auth",
     "photos.apps.PhotosConfig",
     "users.apps.UsersConfig",
     "django_cleanup.apps.CleanupConfig",
-    
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
@@ -58,13 +60,13 @@ INSTALLED_APPS = [
     "django_user_agents",
     "django_htmx",
     "verify_email.apps.VerifyEmailConfig",
-    'cloudinary_storage',
-    'cloudinary',
-    
+    "cloudinary_storage",
+    "cloudinary",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -81,7 +83,10 @@ ROOT_URLCONF = "photobooth.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "photos/templates/photos"), os.path.join(BASE_DIR, "photos/templates/socialaccount")],
+        "DIRS": [
+            os.path.join(BASE_DIR, "photos/templates/photos"),
+            os.path.join(BASE_DIR, "photos/templates/socialaccount"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -100,6 +105,7 @@ WSGI_APPLICATION = "photobooth.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -107,11 +113,17 @@ DATABASES = {
     }
 }
 
+if "DATABASE_URL" in os.environ:
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [ 
+AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
@@ -132,15 +144,21 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "/static/"
-MEDIA_URL = "/media/" 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# tells user where to upload file
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -190,39 +208,37 @@ SOCIALACCOUNT_PROVIDERS = {
 SOCIALACCOUNT_FORMS = {
     "signup": "photos.forms.MyCustomSocialSignupForm",
 }
-ACCOUNT_FORMS = {'signup': 'photos.forms.RegistrationForm'}
+ACCOUNT_FORMS = {"signup": "photos.forms.RegistrationForm"}
 
 
-ACCOUNT_USERNAME_BLACKLIST = ['admin','accounts','profile','category','photo']
+ACCOUNT_USERNAME_BLACKLIST = ["admin", "accounts", "profile", "category", "photo"]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EXPIRE_AFTER = "10h" 
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EXPIRE_AFTER = "10h"
 MAX_RETRIES = 10
 
-                                 
+
 VERIFICATION_SUCCESS_TEMPLATE = "email_verification/email_verification_successful.html"
 
 VERIFICATION_FAILED_TEMPLATE = "email_verification/email_verification_failed.html"
 
 REQUEST_NEW_EMAIL_TEMPLATE = "email_verification/request_new_email.html"
 
-LINK_EXPIRED_TEMPLATE = 'email_verification/link_expired.html' 
+LINK_EXPIRED_TEMPLATE = "email_verification/link_expired.html"
 
-NEW_EMAIL_SENT_TEMPLATE  = 'email_verification/new_email_sent.html'
- 
+NEW_EMAIL_SENT_TEMPLATE = "email_verification/new_email_sent.html"
 
-LOGIN_URL = 'login'
 
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
- 
+LOGIN_URL = "login"
+
+
+# tells user where to upload file
+
+
+# https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
